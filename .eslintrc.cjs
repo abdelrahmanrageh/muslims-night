8c19d3e53e130659898c5b2a3761205d7eb27333
Abdelrahman Rageh <50891852+abdelrahmanrageh@users.noreply.github.com>
2024-09-05 20:07:48 +0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'eslint:recommended', 
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
